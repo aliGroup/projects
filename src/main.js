ce2b1ace071a94347abb2438847318b5ac2278bf
aliGroup <1932593157@qq.com>
2018-12-05 16:53:00 +0800

    第二次修改

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/*引入Vue框架*/
 import Vue from 'vue'
+/*引入根组件*/
 import App from './App'
+/*引入路由设置*/
 import router from './router'
 
+/*关闭生产模式下给出的提示*/
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+/*定义实例*/
 new Vue({
   el: '#app',
   router,
