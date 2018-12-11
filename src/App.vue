9c9a35e3fd64f7902ca6966521cd3c941308c2bf
aliGroup <1932593157@qq.com>
2018-12-11 17:49:57 +0800

    第五次提交

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" alt="">
-    <router-view></router-view>
+    <!--<img src="./assets/logo.png" alt="">-->
+    <!--<router-view></router-view>-->
+    <div id="Container">
+      <div id="bgContainer">
+        <img src="assets/3.ico" alt="查看功能"/>
+        <img src="assets/1.ico" alt="收藏"/>
+        <img src="assets/4.ico" alt="搜索"/>
+      </div>
+      <div></div>
+      <div></div>`
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
 }
 </script>
 
@@ -18,6 +27,26 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width:100%;
+  height:100%;
+  padding:0;
+  margin:0;
 }
+  #Container{
+    width:100%;
+    height:100%;
+    padding:0;
+    margin:0;
+  }
+  #bgContainer{
+    widht:100%;
+    height:95px;
+    background: url("./assets/2.jpg");
+    -webkit-background-size:cover;
+    background-size:cover ;
+  }
+  #bgContainer>img{
+    width:100%;
+    height:100%;
+  }
 </style>
